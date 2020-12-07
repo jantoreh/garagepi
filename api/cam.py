9bfa83777ffb4bc69ed9c88b21b0938ab6b0b96b
jantoreh <jantorehorn@gmail.com>
2020-12-07 19:22:44 +0100

    start preview and use camera port

--- api/cam.py
+++ api/cam.py
@@ -5,12 +5,12 @@ from fastapi.responses import StreamingResponse
 
 camera = PiCamera()
 camera.resolution = (320, 240)
+camera.start_preview()
 
 
 def capture():
     stream = io.BytesIO()
-
-    for _ in camera.capture_continuous(stream, format="png", use_video_port=True):
+    for _ in camera.capture_continuous(stream, format="png", use_video_port=False):
         stream.truncate()
         stream.seek(0)
         return StreamingResponse(stream, media_type="image/png")
