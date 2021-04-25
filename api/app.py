9981e0b71db362481df8b205cbdfcc51aa49310b
jantoreh <jantorehorn@gmail.com>
2021-04-25 21:05:03 +0200

    add garage door trigger

--- api/app.py
+++ api/app.py
@@ -1,19 +1,15 @@
-import io
-
-import numpy as np
 import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 
 from api.cam import convert_image_to_bytes, get_single_image
+from api.garage_doors import trigger_right_garage_door
 
 app = FastAPI()
 
 
 @app.get("/alive")
-async def _capture():
-    image = get_single_image()
-    image = convert_image_to_bytes(image)
+async def _alive():
     return "Hi"
 
 
@@ -22,6 +18,13 @@ async def _capture():
     image = get_single_image()
     image = convert_image_to_bytes(image)
     return StreamingResponse(image, media_type="image/png")
+
+
+@app.get("/door", status_code=200)
+async def _trigger_door():
+    trigger_right_garage_door()
+    return
+
 
 
 if __name__ == "__main__":
---
+++ api/garage_doors.py
@@ -0,0 +1,12 @@
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+RIGHT_DOOR_PIN = 18
+GPIO.setup(RIGHT_DOOR_PIN, GPIO.OUT)
+
+
+def trigger_right_garage_door():
+    GPIO.output(RIGHT_DOOR_PIN, GPIO.HIGH)
+    time.sleep(0.01)
+    GPIO.output(RIGHT_DOOR_PIN, GPIO.LOW)
